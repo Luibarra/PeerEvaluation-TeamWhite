96ebe36ed1a49661f605bbee57af17bc0e9a7452
Luibarra <lai19a@acu.edu>
2022-03-11 09:45:44 -0600

    Ten "Tests"
    added tests

--- PeerEvaluation/src/main/java/PeerEvaluation.java
+++ PeerEvaluation/src/main/java/PeerEvaluation.java
@@ -8,26 +8,36 @@ import java.util.Scanner;
 
 public class PeerEvaluation 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Calculator! AMA." );
-    }
+  public static void main( String[] args )
+  {
+    System.out.println( "" );
+  }
 
-    // Parse CSV Function     
-    public void parseCSV(String input) throws Exception {
+  // Parse CSV Function     
+  public String[][] parseCSV(String input) throws Exception 
+  {
+    String[][] table = new String[100][100]; 
 
-      //parsing a CSV file into the constructor of Scanner class 
-      Scanner sc = new Scanner(new File(input));
-      
-      //setting comma as delimiter pattern
-      sc.useDelimiter(",");
-      
-      while (sc.hasNext()) {
-        System.out.print(sc.next());
+    //parsing a CSV file into the constructor of Scanner class 
+    Scanner sc = new Scanner(new File(input));
+    
+    //setting comma as delimiter pattern
+    sc.useDelimiter(",");
+
+    for(int i = 0; i < 100; i++){
+      for(int j = 0; j < 100; j++){
+        if(sc.hasNext()){
+          String st = sc.next(); 
+          table[i][j] = st; 
+          System.out.print(" " + st); 
+        }
       }
-      System.out.println();
-      sc.close();   //closes the scanner 
+    }
+    System.out.println();
+
+    sc.close();   //closes the scanner 
        
+    return table; 
   }
 }
 
--- PeerEvaluation/src/test/java/PeerEvaluationTest.java
+++ PeerEvaluation/src/test/java/PeerEvaluationTest.java
@@ -9,15 +9,68 @@ import org.junit.*;
 public class PeerEvaluationTest
 {
     @Test
-    public void testBlank() 
+    public void testColumnNames() 
     {
     	try {
         PeerEvaluation PeerEval = new PeerEvaluation(); 
-        PeerEval.parseCSV("evals.csv");
+        String[][] table = PeerEval.parseCSV("evals.csv");
+
+        //check that column names are what they should be 
+        assertEquals("evaluation-id", table[0][0]);
+        assertEquals("rator-id", table[0][1].replaceAll("\\s+",""));
+        assertEquals("ratee-id", table[0][2].replaceAll("\\s+",""));
+        assertEquals("question-id", table[0][3].replaceAll("\\s+",""));
 		  } 
 	
       catch (Exception e) {
-        System.out.println("blab");
+        System.out.println("test fail tho");
+      }
+    }
+
+    @Test
+    public void testRatings() 
+    {
+    	try {
+        PeerEvaluation PeerEval = new PeerEvaluation(); 
+        String[][] table = PeerEval.parseCSV("evals.csv");
+
+        //ensure that each rating is within a range of 1 to 5 
+        assertTrue(Integer.parseInt(table[1][4]) < 6 && Integer.parseInt(table[1][4]) > 0);
+        assertTrue(Integer.parseInt(table[2][4]) < 6 && Integer.parseInt(table[2][4]) > 0);
+        assertTrue(Integer.parseInt(table[3][4]) < 6 && Integer.parseInt(table[3][4]) > 0);
+        assertTrue(Integer.parseInt(table[4][4]) < 6 && Integer.parseInt(table[4][4]) > 0);
+		  } 
+	
+      catch (Exception e) {
+        System.out.println("");
+      }
+    }
+
+    @Test
+    public void testEvalPair() 
+    {
+    	try {
+        PeerEvaluation PeerEval = new PeerEvaluation(); 
+        String[][] table = PeerEval.parseCSV("evals.csv");
+
+        //save eval id 1 and its pair of rator and ratee
+        int eval = Integer.parseInt(table[1][0]);
+        int rator = Integer.parseInt(table[1][1]); 
+        int ratee = Integer.parseInt(table[1][2]); 
+
+        //ensure if the eval has the same id, same pairing
+        if(Integer.parseInt(table[2][0]) == eval)
+            assertTrue(Integer.parseInt(table[1][1]) == rator && Integer.parseInt(table[1][2]) == ratee); 
+
+        if(Integer.parseInt(table[3][0]) == eval)
+            assertTrue(Integer.parseInt(table[3][1]) == rator && Integer.parseInt(table[3][2]) == ratee);
+            
+        if(Integer.parseInt(table[4][0]) == eval)
+            assertTrue(Integer.parseInt(table[4][1]) == rator && Integer.parseInt(table[4][2]) == ratee);
+		  } 
+	
+      catch (Exception e) {
+        System.out.println("");
       }
     }
 }
