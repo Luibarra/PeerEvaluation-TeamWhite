6af3da3f6452cea1106b0a411732c50aa2edced5
Luibarra <lai19a@acu.edu>
2022-03-31 19:04:51 -0500

    Database Connection
    Connection now works and testing or editing the database is working along with functions implemented for that

--- PeerEvaluation/src/main/java/PeerEvaluation.java
+++ PeerEvaluation/src/main/java/PeerEvaluation.java
@@ -6,10 +6,10 @@ import java.io.* ;
 import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 
 public class PeerEvaluation 
@@ -17,6 +17,68 @@ public class PeerEvaluation
   public static void main( String[] args )
   {
     System.out.println( "" );
+  }
+
+  public Connection c;
+    
+  public Connection connect(String url, String user, String password) {
+    try {
+      //Class.forName("org.postgresql.Driver");
+      c = DriverManager.getConnection(url, user, password);
+    } 
+    catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName()+": "+e.getMessage());
+      System.exit(0);
+    }
+      System.out.println("Opened database: " + url + " successfully.");
+      return c;
+  }
+
+  //query function
+  public ResultSet query(String inQuery) throws Exception {
+    // System.out.println("query: [" + inQuery + "]");
+    ResultSet rs = null;
+    PreparedStatement pstmt = c.prepareStatement(inQuery);
+    rs = pstmt.executeQuery();
+    return rs;
+  }
+
+  //nonquery function 
+  public void nonquery(String inQuery)  {
+    // System.out.println("nonquery: [" + inQuery + "]");
+    ResultSet rs;
+    try {
+        PreparedStatement pstmt = c.prepareStatement(inQuery);
+        rs = pstmt.executeQuery();
+    }  
+    catch(Exception e) {
+      if (! ("No results were returned by the query.".equals(e.getMessage()))) 
+      {
+        System.out.println("ERROR response delete");
+        System.out.println(e.getMessage());
+      }
+    }
+  }
+        
+  public void v_response_print(ResultSet rs) {
+    System.out.println("evalid\tstudent1\tstudent2\tcategory\tvalue");
+    try {
+        while(rs.next()) {
+            System.out.print(rs.getInt(1));
+            System.out.print("\t");
+            System.out.print(rs.getInt(2));
+            System.out.print("\t\t");
+            System.out.print(rs.getInt(3));
+            System.out.print("\t\t"+ rs.getString(4));
+            System.out.print("\t\t");
+            System.out.println(rs.getInt(5));
+        } 
+    }
+    catch(Exception exec) {
+        System.out.println("v_response_print not happy");
+        exec.printStackTrace();
+    }
   }
 
   // Parse CSV Function     
@@ -35,7 +97,7 @@ public class PeerEvaluation
         if(sc.hasNext()){
           String st = sc.next(); 
           table[i][j] = st; 
-          System.out.print(" " + st); 
+          // System.out.print(" " + st);    //printing out csv info
         }
       }
     }
@@ -46,19 +108,19 @@ public class PeerEvaluation
     return table; 
   }
 
-  public Connection getConnection() throws SQLException {
+  // public Connection getConnection() throws SQLException {
 
-    Connection conn = null;
-    Properties connectionProps = new Properties();
-    connectionProps.put("user", "postgres");
-    connectionProps.put("password", "Spartan7624$$");
+  //   Connection conn = null;
+  //   Properties connectionProps = new Properties();
+  //   connectionProps.put("user", "postgres");
+  //   connectionProps.put("password", "Spartan7624$$");
 
-    String url = "jdbc:postgresql://localhost:5432/CS735v1"; 
-    conn = DriverManager.getConnection(url, connectionProps);
+  //   String url = "jdbc:postgresql://localhost:5432/CS735v1"; 
+  //   conn = DriverManager.getConnection(url, connectionProps);
     
-    System.out.println("Connected to database");
-    return conn;
-}
+  //   System.out.println("Connected to database");
+  //   return conn;
+  // }
 }
 
 
--- PeerEvaluation/src/test/java/PeerEvaluationTest.java
+++ PeerEvaluation/src/test/java/PeerEvaluationTest.java
@@ -1,6 +1,16 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * 
@@ -8,38 +18,97 @@ import org.junit.*;
 
 public class PeerEvaluationTest
 {
-    @Test
-    public void testDBConnection() 
-    {
-      try {
-        PeerEvaluation PeerEval = new PeerEvaluation();
-        PeerEval.getConnection();
-      } 
+  public static PeerEvaluation pc;
+  public static Connection c;
 
-      catch (Exception e) {
-        System.out.println("test fail tho");
-      }
+  @BeforeClass
+  public static void setUpDB() throws Exception {
+    System.out.println("connecting...");
+    pc = new PeerEvaluation();
+    c = pc.connect("jdbc:postgresql://localhost:5432/cs375v1", "mrblee", "purplewhite");
+  }
+
+  @AfterClass
+  public static void teardownThisWall() {
+   System.out.println(); 
+   System.out.println("all done");
+  }
+
+  public void interacting_delete() {
+    pc.nonquery("delete from interacting");
+  }
+
+  public void interacting_inserts() {
+    String insert = "INSERT INTO Interacting (levelID, Attribute1, Attribute2, Attribute3, Selected) VALUES ('1', 'Asks for and shows an interest in teammates ideas and contributions.', 'Makes sure teammates stay informed and understand each other.', 'Provides encouragement or enthusiasm to the team.', '0');"; 
+    pc.nonquery(insert);
+  }
+        
+  public int count_rows (String table) {
+    ResultSet rs;
+    int n = -1;
+
+    try {
+        rs = pc.query("select count(*) as n from " + table);
+    } 
+    catch (Exception e) {
+        System.out.println("ERROR select count(*) as n: " + e.getMessage());
+        assertTrue(false);
+        return -1;
     }
+
+    try {
+        rs.next();
+        n = rs.getInt("n");
+    } 
+    catch (Exception e) {
+        System.out.println("ERROR rs.next() and getInt()");
+        assertTrue(false);
+        return -1;
+    }
+
+    return n;
+  }
+
+  @Test
+  public void check_delete () {
+    int n = -1;
+    interacting_delete();
+    n = count_rows("Interacting");
+    assertEquals("Interacting table should be empty", 0, n);
+    System.out.print("delete from code functioning...");
+  }
+
+  @Test
+  public void check_inserts () {
+    int n = -1;
+    interacting_delete();
+    interacting_inserts();
+    n = count_rows("Interacting");
+    assertEquals("should now be 1", 1, n);
+    System.out.print("insert from code functioning...");
+  }    
   
-    @Test
-    public void testColumnNames() 
-    {
-    	try {
-        PeerEvaluation PeerEval = new PeerEvaluation(); 
-        String[][] table = PeerEval.parseCSV("evals.csv");
+  @Test
+  public void testColumnNames() 
+  {
+    try {
+      PeerEvaluation PeerEval = new PeerEvaluation(); 
+      String[][] table = PeerEval.parseCSV("evals.csv");
 
-        //check that column names are what they should be 
-        assertEquals("evaluation-id", table[0][0]);
-        assertEquals("rator-id", table[0][1].replaceAll("\\s+",""));
-        assertEquals("ratee-id", table[0][2].replaceAll("\\s+",""));
-        assertEquals("question-id", table[0][3].replaceAll("\\s+",""));
-		  } 
-	
-      catch (Exception e) {
-        System.out.println("test fail tho");
-      }
+      //check that column names are what they should be 
+      assertEquals("evaluation-id", table[0][0]);
+      assertEquals("rator-id", table[0][1].replaceAll("\\s+",""));
+      assertEquals("ratee-id", table[0][2].replaceAll("\\s+",""));
+      assertEquals("question-id", table[0][3].replaceAll("\\s+",""));
+    } 
+
+    catch (Exception e) {
+      System.out.println("test fail tho");
     }
+  }
 
+  // ======== this was breaking for no reason ==========
+  
     // @Test
     // public void testRatings() 
     // {
