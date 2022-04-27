43c4b33b648df7aa45b299c1fdceb78361a72acc
fishercoburn1 <73913533+fishercoburn1@users.noreply.github.com>
2022-04-26 23:23:33 -0500

    Added eval 2 teams

--- PeerEvaluation/src/main/resources/dbcreate.sql
+++ PeerEvaluation/src/main/resources/dbcreate.sql
@@ -119,7 +119,18 @@ insert into teams (evalID,teamID,student) values (1,5,15);
 insert into teams (evalID,teamID,student) values (1,6,16);
 insert into teams (evalID,teamID,student) values (1,6,17);
 insert into teams (evalID,teamID,student) values (1,7,18);
-insert into teams (evalID,teamID,student) values (1,7,19);
+insert into teams (evalID,teamID,student) values (2,7,19);
+insert into teams (evalID,teamID,student) values (2,1,2);
+insert into teams (evalID,teamID,student) values (2,1,3);
+insert into teams (evalID,teamID,student) values (2,2,1);
+insert into teams (evalID,teamID,student) values (2,2,4);
+insert into teams (evalID,teamID,student) values (2,3,5);
+insert into teams (evalID,teamID,student) values (2,3,6);
+insert into teams (evalID,teamID,student) values (2,4,7);
+insert into teams (evalID,teamID,student) values (2,4,8);
+insert into teams (evalID,teamID,student) values (2,5,9);
+insert into teams (evalID,teamID,student) values (2,5,10);
+
 
 
 -- Response Inserts 
