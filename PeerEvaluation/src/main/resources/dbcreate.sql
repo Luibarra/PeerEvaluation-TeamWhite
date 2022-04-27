1692e9a30030b85395b2c48d5f1279784a3eb824
Luibarra <lai19a@acu.edu>
2022-04-26 23:32:03 -0500

    extra line in dbcreate

--- PeerEvaluation/src/main/resources/dbcreate.sql
+++ PeerEvaluation/src/main/resources/dbcreate.sql
@@ -119,7 +119,6 @@ insert into teams (evalID,teamID,student) values (1,5,15);
 insert into teams (evalID,teamID,student) values (1,6,16);
 insert into teams (evalID,teamID,student) values (1,6,17);
 insert into teams (evalID,teamID,student) values (1,7,18);
-insert into teams (evalID,teamID,student) values (2,7,19);
 insert into teams (evalID,teamID,student) values (2,1,2);
 insert into teams (evalID,teamID,student) values (2,1,3);
 insert into teams (evalID,teamID,student) values (2,2,1);
