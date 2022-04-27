5c0cb28472f4140ec835529561048604f5a9e876
fishercoburn1 <73913533+fishercoburn1@users.noreply.github.com>
2022-04-26 22:32:57 -0500

    Included insertions to tables

--- PeerEvaluation/src/main/resources/dbcreate.sql
+++ PeerEvaluation/src/main/resources/dbcreate.sql
@@ -431,6 +431,42 @@ insert into Response (evalID,student1,student2,category,val) values (1,15,13,'H'
 insert into Response (evalID,student1,student2,category,val) values (1,15,14,'H',4);
 insert into Response (evalID,student1,student2,category,val) values (1,15,15,'H',4);
 
+--Insertions into course table
+insert into course (id, abbrev, title) values (123, BIBL, Bible);
+insert into course (id, abbrev, title) values (234, ACCT, Accounting);
+insert into course (id, abbrev, title) values (345, FIN, Finance);
+insert into course (id, abbrev, title) values (456, BIBT, BibleTheory);
+insert into course (id, abbrev, title) values (567, PHYS, Physiology);
+insert into course (id, abbrev, title) values (678, DET, Gaming);
+insert into course (id, abbrev, title) values (789, CS, CompSci);
+
+--Insertions into section table
+insert into section (id, course_id, title, start) values (11, 123, BibleClass, 2008-11-11);
+insert into section (id, course_id, title, start) values (12, 234, AccountingClass, 2008-11-11);
+insert into section (id, course_id, title, start) values (1, 345, FinanceClass, 2008-11-11);
+insert into section (id, course_id, title, start) values (2, 456, BibleTheoryClass, 2008-11-11);
+insert into section (id, course_id, title, start) values (3, 567, PhysiologyClass, 2008-11-11);
+insert into section (id, course_id, title, start) values (4, 678, GamingClass, 2008-11-11);
+insert into section (id, course_id, title, start) values (5, 789, CompSciClass, 2008-11-11);
+
+--Insertions into eval table
+insert into eval (id, title, end_date) values (1, first, 2008-11-20);
+insert into eval (id, title, end_date) values (2, second, 2008-11-20);
+insert into eval (id, title, end_date) values (3, third, 2008-11-20);
+insert into eval (id, title, end_date) values (4, fourth, 2008-11-20);
+insert into eval (id, title, end_date) values (5, fifth, 2008-11-20);
+insert into eval (id, title, end_date) values (6, sixth, 2008-11-20);
+insert into eval (id, title, end_date) values (7, seventh, 2008-11-20);
+
+--Insertions into eval_section table
+insert into eval (eval_id, section_id) values (1, 11);
+insert into eval (eval_id, section_id) values (2, 12);
+insert into eval (eval_id, section_id) values (3, 1);
+insert into eval (eval_id, section_id) values (4, 2);
+insert into eval (eval_id, section_id) values (5, 3);
+insert into eval (eval_id, section_id) values (6, 4);
+insert into eval (eval_id, section_id) values (7, 5);
+
 -- INSERT INTO TeamContribution (levelID, Attribute1, Attribute2, Attribute3, Selected)
 -- VALUES ('1', 'Does more or higher-quality work than expected', ' Makes important contributions that improve the teams work.', 'Helps teammates who are having difficulty completing their work.', '0');
 -- INSERT INTO TeamContribution (levelID, Attribute1, Attribute2, Attribute3, Selected)
